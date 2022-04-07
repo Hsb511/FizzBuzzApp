283ccd27ff57bd5d062758c5fd2045bb0a98ae91
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-04-07 22:19:59 +0200

    [UPD] Setting the gradle files with compose & hilt dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.example.fizzbuzzapp"
+        minSdk = 26
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Core libraries
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+
+    // Compose libraries
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha05")
+    implementation("androidx.compose.compiler:compiler:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.ui:ui:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.ui:ui-viewbinding:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.compose.material:material:${rootProject.extra["composeVersion"]}")
+    implementation("androidx.navigation:navigation-compose:2.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["composeVersion"]}")
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.40.5")
+    kapt("com.google.dagger:hilt-android-compiler:2.40.5")
+
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+kapt {
+    correctErrorTypes = true
+}
