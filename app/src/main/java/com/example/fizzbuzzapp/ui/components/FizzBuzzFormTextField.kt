f5cb13776a0302422b402902bf965a2f0c7c83db
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-04-13 21:58:39 +0200

    [ADD] Close keyboard when field is OK

--- app/src/main/java/com/example/fizzbuzzapp/ui/components/FizzBuzzFormTextField.kt
+++ app/src/main/java/com/example/fizzbuzzapp/ui/components/FizzBuzzFormTextField.kt
@@ -2,15 +2,19 @@ package com.example.fizzbuzzapp.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 inline fun FizzBuzzFormTextField(
     textFieldValue: MutableState<TextFieldValue>,
@@ -18,6 +22,7 @@ inline fun FizzBuzzFormTextField(
     isNumber: Boolean,
     crossinline onValueChange: (textFieldValue: TextFieldValue) -> String
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     OutlinedTextField(
         value = textFieldValue.value,
         onValueChange = {
@@ -32,6 +37,9 @@ inline fun FizzBuzzFormTextField(
         } else {
             KeyboardOptions(keyboardType = KeyboardType.Text)
         },
+        keyboardActions = KeyboardActions(onDone = {
+            keyboardController?.hide()
+        }),
         label = { Text(label) },
         isError = if (isNumber) {
             textFieldValue.value.text.isBlank()
