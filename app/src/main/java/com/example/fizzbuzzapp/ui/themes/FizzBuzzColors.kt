01a968d583817f37dde6f70d02da0a485c470d15
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-06-13 17:16:58 +0200

    [UPD] Changing the colors depending on the logo

--- app/src/main/java/com/example/fizzbuzzapp/ui/themes/FizzBuzzColors.kt
+++ app/src/main/java/com/example/fizzbuzzapp/ui/themes/FizzBuzzColors.kt
@@ -6,25 +6,27 @@ import androidx.compose.ui.graphics.Color
 
 
 val Turquoise = Color(0xFF1EDDCD)
-val Olivine = Color(0xFFADC178)
+val Olivine = Color(0xFFA9D18E)
 val IndianRed = Color(0xFFE15A58)
 val MiddleRedPurple = Color(0xFF994B47)
 val SandyBrown = Color(0xFFF4A259)
 val GoldCrayola = Color(0xFFEDC298)
-val CharCoal = Color(0xFF3E4C59)
+val CharCoal = Color(0xFF033334)
 
 val FizzBuzzLightColors = lightColors(
-    primary = MiddleRedPurple,
-    primaryVariant = IndianRed,
+    primary = Olivine,
+    primaryVariant = Turquoise,
     secondary = SandyBrown,
-    secondaryVariant = Olivine,
+    secondaryVariant = MiddleRedPurple,
     onPrimary = Color.White
 )
 
 val FizzBuzzDarkColors = darkColors(
-    primary = GoldCrayola,
-    primaryVariant = SandyBrown,
-    secondary = Turquoise,
-    secondaryVariant = CharCoal,
+    primary = Olivine,
+    primaryVariant = Turquoise,
+    secondary = GoldCrayola,
+    secondaryVariant = IndianRed,
+    background = CharCoal,
+    surface = CharCoal,
     onPrimary = Color.Black
 )
